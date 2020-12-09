e806bb0a36e712ca1d117175946583317a62fe0d
Ricardo Alonzo Ugalde <34896374+ricardo-alonzo-ugalde-EE@users.noreply.github.com>
2020-12-08 19:51:15 -0800

    visual changes.added splash screen

---
+++ FinalProject/app/src/main/java/com/example/finalproject/SplashActivity.java
@@ -0,0 +1,41 @@
+package com.example.finalproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Created by Sumeet Jain on 23-06-2018.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+
+    private ImageView logo;
+    private static int splashTimeOut=5000;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash3);
+        logo=(ImageView)findViewById(R.id.logo);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },splashTimeOut);
+
+        Animation myanim = AnimationUtils.loadAnimation(this,R.anim.mysplashanimation);
+        logo.startAnimation(myanim);
+    }
+}
